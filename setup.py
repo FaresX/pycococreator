dc31d637e2a5038d8ae3b07d836b91b209d07d02
Faresx <39757092+Faresx@users.noreply.github.com>
2019-12-02 20:03:19 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from distutils.extension import Extension
+
+# To install library to Python site-packages run "python setup.py install"
+
+setup(name='pycococreatortools',
+    packages=['pycococreatortools'],
+    package_dir = {'pycococreatortools': 'pycococreatortools'},
+    version='0.1.0',
+    description = 'Tools to create COCO datasets',
+    url = 'https://github.com/Faresx/pycococreator',
+    author = 'fares',
+    author_email = 'fyzxst@gmail.com',
+    download_url = 'https://github.com/Faresx/pycococreator/archive/0.1.0.tar.gz',
+    keywords = ['coco', 'dataset', 'machine-learning'],
+    install_requires=[
+        'numpy', 'pillow', 'scikit-image', 'json'
+    ],
+)
