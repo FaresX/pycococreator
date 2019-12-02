8c5e47190432bdc91c8d49a4527b382486f5f584
Faresx <fyzxst@sina.com>
2019-12-02 22:44:15 +0800

    debug

--- test.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-import pycococreatortools as pct
-home = "/home/fares/testmask/shapes/train/"
-pct.cut2trte(home+"/shapes_train2018", home+"/annotation", home+"/cut", rm=True)
